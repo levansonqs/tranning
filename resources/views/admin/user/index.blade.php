7dd9b78cc15c03aba271f4b1369870e5b9257fd7
Phuoc95 <duyphuoctk@gmail.com>
2018-03-15 09:57:26 +0700

    edit user

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -34,8 +34,11 @@
  							<thead>
  								<tr>
  									<th>ID</th>
- 									<th>Tên đăng nhập</th> 									
+ 									<th>Tên đăng nhập</th>
  									<th>Họ tên</th> 									
+ 									<th>Email</th> 									
+ 									<th>Avatar</th> 									
+ 									<th>Cấp độ</th> 									
  									<th>Chức năng</th>
  								</tr>
  							</thead>
@@ -47,11 +50,58 @@
  								@php
  								$urlEdit = route('admin.user.edit',[$item->id]);
  								$urlDel = route('admin.user.delete',[$item->id]);
+
+ 								
  								@endphp
  								<tr>
  									<td>{{ $stt++ }}</td>
  									<td>{{ $item->username }}</td> 								
- 									<td>{{ $item->fullname }}</td> 									
+ 									<td>{{ $item->fullname }}</td> 								
+ 									<td>{{ $item->email }}</td> 									
+ 									<td>
+ 										@php
+ 										$check = strpos($item->avatar,'http');
+ 										if($check == false ){
+ 											if(empty($item->avatar)){
+ 												$picName = "images/userDefault.png";		
+ 												$picUrl  = Storage::url($picName);
+ 											}else{
+ 												$picName = "images/".$item->avatar;		
+ 												$picUrl  = Storage::url($picName);
+ 											}
+ 										}else{
+ 											$picUrl = "$item->avatar";
+ 										}
+
+ 											// if(empty($item->avatar)){
+ 											// 	$picName = "images/userDefault.png";		
+ 											// 	$picUrl  = Storage::url($picName);
+ 											// }elseif( strpos($item->avatar,'http://') ){
+ 											// 	$picUrl = $item->avatar;
+ 											// 	// dd($picUrl);
+ 											// }
+ 											// else{
+ 											// 	$picName = "images/".$item->avatar;		
+ 											// 	$picUrl  = Storage::url($picName);
+ 											// }
+ 										@endphp
+ 										<img src="{{ $picUrl}}" class="img-responsive thumbnail">
+ 										{{ dump($check)}}
+ 									</td> 
+ 									<td>
+ 										@php
+ 										switch ($item->level) {
+ 											case '1':
+ 											echo "Admin";
+ 											break;
+ 											case '2' :
+ 											echo  "Quản lý";
+ 											default:
+ 											echo "Khách hàng";
+ 											break;
+ 										}
+ 										@endphp
+ 									</td> 									
  									<td class="text-left">
  										<a href="{{ $urlEdit }}" class="btn btn-primary btn-sm btn-del editItem "><i class="fa fa-edit"></i>  Sửa</a>
  										<a href="{{ $urlDel }}" class="btn btn-danger btn-sm btn-del delItem "><i class="fa fa-trash"></i>  Xóa</a>
